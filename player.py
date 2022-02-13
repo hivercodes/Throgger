cd12965dcbeea3b609215d5f2119e5830adee839
hivercodes <lars.eriksson.mail@gmail.com>
2022-02-13 14:52:55 +0100

    Added the player turtle and got it moving at press of the Up button.

--- player.py
+++ player.py
@@ -1,7 +1,23 @@
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+from turtle import Turtle
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("Black")
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+
+
+    def move(self):
+        self.forward(10)
+
+
+
